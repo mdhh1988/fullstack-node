d3cb02185336d3fe8f0d3eb78f12ab7bf17651fa
Myu1533 <diabolos.man@gmail.com>
2025-05-04 07:44:48 +0800

    mod: 修改index文件，文件引入路径

--- index.js
+++ index.js
@@ -1,36 +1,10 @@
 import { createRandomPicker} from './lib/random.js';
 import { generate } from './lib/generator.js';
-import commandLineArgs from 'command-line-args';
-import commandLineUsage from 'command-line-usage';
-
-// 读取命令行参数
-function parseOptions(option = {}) {
-  const argv = process.argv;
-  for(let i = 2; i < argv.length; i++) {
-    const cmd = argv[i-1];
-    const value = argv[i];
-    if(cmd==='--title') {
-      options.title = value;
-    } else if(cmd === '--min') {
-      options.min = Number(value);
-    } else if(cmd ==='--max') {
-      options.max = Number(value);
-    }
-  }
-  return options;
-}
-
-
-
-
+import {options} from './lib/cmd.js';
+import { loadCorpus, saveCorpus } from './lib/corpus.js';
 
 const corpus = loadCorpus('corpus/data.json');
-
-if('help' in options) {
-  console.log(usage);
-} else {
-  const title = options.title || createRandomPicker(corpus.title)();
-  const article = generate(title, {corpus, ...options});
-  const output = saveCorpus(title, article);
-  console.log(`文章已保存到 ${output}`);
-}
+const title = options.title || createRandomPicker(corpus.title)();
+const article = generate(title, {corpus, ...options});
+const output = saveCorpus(title, article);
+console.log(`文章已保存到 ${output}`);
