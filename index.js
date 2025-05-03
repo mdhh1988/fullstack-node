f1ebd7286e4c3ba8e9d034a1a53bdd4024c5a296
Myu1533 <diabolos.man@gmail.com>
2025-05-04 07:41:47 +0800

    mod: 新增cmd文件，用于处理cmd相关交互功能

--- index.js
+++ index.js
@@ -22,43 +22,10 @@ function parseOptions(option = {}) {
 
 
 
-// 定义帮助文档内容
-const sections = [
-  {
-    header: '文章生成器',
-    content: '根据给定的标题生成一篇文章'
-  },
-  {
-    header: '选项',
-    optionList: [
-      {
-        name: 'title',
-        typeLabel: '{underline title}',
-        description: '文章标题'
-      },
-      {
-        name: 'min',
-        typeLabel: '{underline min}',
-        description: '最小段落数'
-      },
-      {
-        name: 'max',
-        typeLabel: '{underline max}',
-        description: '最大段落数'
-      }
-    ]
-  }
-]
-const usage = commandLineUsage(sections);
+
 
 const corpus = loadCorpus('corpus/data.json');
-const optionDefinitions = [
-  { name: 'help' },
-  { name: 'title', type: String },
-  { name: 'min', type: Number },
-  { name: 'max', type: Number }
-];
-const options = commandLineArgs(optionDefinitions);
+
 if('help' in options) {
   console.log(usage);
 } else {
